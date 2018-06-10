75a69d1a52839f53e060da0b248addb5ed415e78
Chiman-Dev <bchiman.subudhi@gmail.com>
2018-06-10 20:37:32 +0530

    first commit

---
+++ assign4.js
@@ -0,0 +1,19 @@
+var largest=num1 = -8;
+var num2 = 12.5;
+var num3 = -6.769;
+var num4 = 0;
+var num5 = -1;
+
+if (num2>largest) {
+	largest=num2;
+}
+if (num3>largest) {
+	largest=num3;
+}
+if (num4>largest) {
+	largest=num4;
+}
+if (num5>largest) {
+	largest=num5;
+}
+console.log(largest)
